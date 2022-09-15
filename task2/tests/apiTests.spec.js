d8767a4ec0b83bc8b7143bdc2fea964fea7e6999
romanko <Vladyslavr@residenthome.com>
2022-09-15 17:36:01 +0400

    add api test

---
+++ task2/tests/apiTests.spec.js
@@ -0,0 +1,46 @@
+const { test, expect } = require('@playwright/test');
+const URL = 'https://rctest-clone-1625730023-rc.expoplatform.com/api/v1/community/exhibitors';
+let apiContext;
+
+test.beforeAll(async ({ playwright }) => {
+    apiContext = await playwright.request.newContext({
+        baseURL: 'https://rctest-clone-1625730023-rc.expoplatform.com',
+        extraHTTPHeaders: {
+            'Accept': '/api/v1/community/exhibitors',
+        },
+    });
+})
+
+test('API test', async ({ page }) => {
+
+    await test.step('API POST, checking keys data and compare key with value', async () => {
+        const _response = await apiContext.post(`${URL}`, {});
+        expect(_response.ok()).toBeTruthy();
+
+        const res = await _response.json();
+        const data = res.data;
+        let result;
+
+        data.list.map(item => {
+            function compareKeysAndValues (keyToCompare, valueToCompare){
+                const newArr = Object.keys(item).filter(key => item[key] === valueToCompare);
+                const keyAvailable = newArr.some(element => keyToCompare == element);
+                if (keyAvailable){
+                    return true;
+                }else{
+                    return false;
+                }
+            };
+
+            result = compareKeysAndValues('name', 'testss');
+            console.log('Result API', result);
+
+            const values = Object.values(item);
+            values.map(element => {
+                console.log(typeof element);
+            });
+
+        });
+        await expect(result).toBe(true);
+    });
+});
